514d88c1367ce926168a399c56db365c2958fb6a
ShaharTamir <shahar.tamir@infinityLabs.co.il>
2023-06-24 19:32:47 +0300

    fixed css for israel vectors, added back button and fadein/out animation, added animation for israel vectors to move in

---
+++ static/javascript/why_eilat.js
@@ -0,0 +1,87 @@
+    start_pos = [
+{ 'left': '586px', 'top': '1023px' }, { 'left': '506px', 'top': '1025px' }, { 'left': '1224px', 'top': '979px' }, { 'left': '873px', 'top': '946px' }, { 'left': '737px', 'top': '982px' },
+{ 'left': '1259px', 'top': '947px' }, { 'left': '176px', 'top': '860px' }, { 'left': '1684px', 'top': '929px' }, { 'left': '1814px', 'top': '933px' }, { 'left': '1116px', 'top': '996px' },
+{ 'left': '231px', 'top': '1010px' }, { 'left': '184px', 'top': '629px' }, { 'left': '202px', 'top': '21px' }, { 'left': '1817px', 'top': '44px' }, { 'left': '1828px', 'top': '794px' },
+{ 'left': '666px', 'top': '30px' }, { 'left': '1012px', 'top': '52px' }, { 'left': '1130px', 'top': '91px' }, { 'left': '62px', 'top': '599px' }, { 'left': '33px', 'top': '559px' },
+{ 'left': '81px', 'top': '400px' }, { 'left': '366px', 'top': '21px' }, { 'left': '1682px', 'top': '585px' }, { 'left': '1534px', 'top': '99px' }, { 'left': '122px', 'top': '652px' },
+{ 'left': '641px', 'top': '-3px' }, { 'left': '1642px', 'top': '10px' }, { 'left': '20px', 'top': '91px' }, { 'left': '1677px', 'top': '79px' }, { 'left': '176px', 'top': '107px' },
+{ 'left': '729px', 'top': '26px' }, { 'left': '573px', 'top': '49px' }, { 'left': '1159px', 'top': '60px' }, { 'left': '1593px', 'top': '933px' }, { 'left': '252px', 'top': '785px' },
+{ 'left': '305px', 'top': '74px' }, { 'left': '71px', 'top': '662px' }, { 'left': '122px', 'top': '881px' }, { 'left': '71px', 'top': '850px' }, { 'left': '248px', 'top': '949px' },
+{ 'left': '388px', 'top': '939px' }, { 'left': '24px', 'top': '838px' }, { 'left': '214px', 'top': '886px' }, { 'left': '625px', 'top': '957px' }, { 'left': '184px', 'top': '748px' },
+{ 'left': '155px', 'top': '956px' }, { 'left': '62px', 'top': '903px' }, { 'left': '27px', 'top': '621px' }, { 'left': '456px', 'top': '978px' }, { 'left': '1855px', 'top': '1014px' },
+{ 'left': '73px', 'top': '990px' }, { 'left': '394px', 'top': '1023px' }, { 'left': '757px', 'top': '1011px' }, { 'left': '1438px', 'top': '999px' }, { 'left': '674px', 'top': '1001px' },
+{ 'left': '1852px', 'top': '874px' }, { 'left': '1156px', 'top': '1032px' }, { 'left': '1524px', 'top': '986px' }, { 'left': '1794px', 'top': '347px' }, { 'left': '1412px', 'top': '971px' },
+{ 'left': '546px', 'top': '30px' }, { 'left': '1829px', 'top': '267px' }, { 'left': '1755px', 'top': '130px' }, { 'left': '1315px', 'top': '78px' }, { 'left': '1593px', 'top': '135px' },
+{ 'left': '1858px', 'top': '91px' }, { 'left': '1643px', 'top': '106px' }, { 'left': '1704px', 'top': '232px' }, { 'left': '472px', 'top': '-15px' }, { 'left': '145px', 'top': '438px' },
+{ 'left': '206px', 'top': '121px' }, { 'left': '1399px', 'top': '82px' }, { 'left': '365px', 'top': '845px' }, { 'left': '1741px', 'top': '40px' }, { 'left': '1076px', 'top': '32px' },
+{ 'left': '1699px', 'top': '669px' }, { 'left': '129px', 'top': '326px' }, { 'left': '960px', 'top': '-5px' }, { 'left': '462px', 'top': '79px' }, { 'left': '1563px', 'top': '1005px' },
+{ 'left': '1041px', 'top': '1027px' }, { 'left': '1237px', 'top': '17px' }, { 'left': '296px', 'top': '176px' }, { 'left': '1209px', 'top': '14px' }, { 'left': '1461px', 'top': '44px' },
+{ 'left': '1509px', 'top': '-3px' }, { 'left': '40px', 'top': '4px' }, { 'left': '102px', 'top': '156px' }, { 'left': '105px', 'top': '249px' }, { 'left': '106px', 'top': '479px' },
+{ 'left': '80px', 'top': '768px' }, { 'left': '17px', 'top': '321px' }, { 'left': '1300px', 'top': '1007px' }, { 'left': '1704px', 'top': '361px' }, { 'left': '900px', 'top': '1018px' },
+{ 'left': '1744px', 'top': '520px' }, { 'left': '794px', 'top': '14px' }, { 'left': '106px', 'top': '553px' }, { 'left': '57px', 'top': '736px' }, { 'left': '17px', 'top': '178px' },
+{ 'left': '534px', 'top': '959px' }, { 'left': '1657px', 'top': '847px' }, { 'left': '1816px', 'top': '403px' }, { 'left': '73px', 'top': '78px' }, { 'left': '1753px', 'top': '642px' },
+{ 'left': '1672px', 'top': '775px' }, { 'left': '1801px', 'top': '852px' }, { 'left': '1697px', 'top': '1023px' }, { 'left': '1789px', 'top': '574px' }, { 'left': '1697px', 'top': '423px' },
+{ 'left': '1741px', 'top': '779px' }, { 'left': '1875px', 'top': '603px' }, { 'left': '1001px', 'top': '1014px' }, { 'left': '302px', 'top': '943px' }, { 'left': '1879px', 'top': '813px' },
+{ 'left': '1836px', 'top': '734px' }, { 'left': '1561px', 'top': '949px' }, { 'left': '1855px', 'top': '202px' }, { 'left': '1839px', 'top': '539px' }, { 'left': '1796px', 'top': '135px' },
+{ 'left': '27px', 'top': '463px' }, { 'left': '1866px', 'top': '477px' }, { 'left': '1791px', 'top': '221px' }, { 'left': '1751px', 'top': '951px' }, { 'left': '1338px', 'top': '21px' },
+{ 'left': '1588px', 'top': '873px' }, { 'left': '1059px', 'top': '17px' }, { 'left': '819px', 'top': '58px' }, { 'left': '1386px', 'top': '38px' }, { 'left': '1682px', 'top': '156px' },
+]
+
+    end_pos = [
+{ 'left': '589.71px', 'top': '873.66px' }, { 'left': '598.53px', 'top': '874.66px' }, { 'left': '598.53px', 'top': '854.86px' }, { 'left': '595.76px', 'top': '791.04px' }, { 'left': '629.86px', 'top': '791.04px' },
+{ 'left': '629.86px', 'top': '710.50px' }, { 'left': '599.67px', 'top': '635.81px' }, { 'left': '650.64px', 'top': '585.02px' }, { 'left': '662.37px', 'top': '466.09px' }, { 'left': '595.76px', 'top': '819.84px' },
+{ 'left': '565.29px', 'top': '845.90px' }, { 'left': '442.69px', 'top': '538.50px' }, { 'left': '532.93px', 'top': '200.06px' }, { 'left': '655.97px', 'top': '171.52px' }, { 'left': '655.97px', 'top': '197.61px' },
+{ 'left': '655.97px', 'top': '206.32px' }, { 'left': '641.10px', 'top': '242.72px' }, { 'left': '668.41px', 'top': '263.85px' }, { 'left': '417.12px', 'top': '485.55px' }, { 'left': '402.23px', 'top': '466.70px' },
+{ 'left': '410.53px', 'top': '414.97px' }, { 'left': '459.09px', 'top': '358.76px' }, { 'left': '495.64px', 'top': '349.20px' }, { 'left': '524.31px', 'top': '256.94px' }, { 'left': '467.35px', 'top': '599.49px' },
+{ 'left': '650.46px', 'top': '171.52px' }, { 'left': '620.02px', 'top': '282.37px' }, { 'left': '522.49px', 'top': '256.94px' }, { 'left': '532.93px', 'top': '249.47px' }, { 'left': '515.83px', 'top': '304.58px' },
+{ 'left': '612.11px', 'top': '206.32px' }, { 'left': '612.11px', 'top': '231.53px' }, { 'left': '613.13px', 'top': '251.32px' }, { 'left': '539.57px', 'top': '387.48px' }, { 'left': '539.57px', 'top': '427.47px' },
+{ 'left': '425.32px', 'top': '485.55px' }, { 'left': '453.18px', 'top': '552.76px' }, { 'left': '501.47px', 'top': '634.61px' }, { 'left': '545.01px', 'top': '641.88px' }, { 'left': '519.66px', 'top': '658.60px' },
+{ 'left': '564.53px', 'top': '745.82px' }, { 'left': '539.19px', 'top': '689.07px' }, { 'left': '547.41px', 'top': '597.49px' }, { 'left': '639.70px', 'top': '635.81px' }, { 'left': '639.70px', 'top': '635.81px' },
+{ 'left': '577.41px', 'top': '819.84px' }, { 'left': '577.41px', 'top': '780.37px' }, { 'left': '564.52px', 'top': '780.37px' }, { 'left': '589.47px', 'top': '710.50px' }, { 'left': '567.64px', 'top': '712.51px' },
+{ 'left': '548px', 'top': '713.25px' }, { 'left': '583.37px', 'top': '780.37px' }, { 'left': '589.46px', 'top': '710.50px' }, { 'left': '567.64px', 'top': '669.30px' }, { 'left': '603.32px', 'top': '669.30px' },
+{ 'left': '620.96px', 'top': '635.81px' }, { 'left': '609.19px', 'top': '466.09px' }, { 'left': '587.92px', 'top': '440.85px' }, { 'left': '609.19px', 'top': '421.89px' }, { 'left': '662.36px', 'top': '437.09px' },
+{ 'left': '662.37px', 'top': '386.44px' }, { 'left': '612.79px', 'top': '338.50px' }, { 'left': '668.77px', 'top': '339.55px' }, { 'left': '650.25px', 'top': '307.05px' }, { 'left': '634.31px', 'top': '251.32px' },
+{ 'left': '650.25px', 'top': '281.92px' }, { 'left': '673.62px', 'top': '311.57px' }, { 'left': '587.25px', 'top': '258.16px' }, { 'left': '564.55px', 'top': '231.53px' }, { 'left': '538.14px', 'top': '333.31px' },
+{ 'left': '522.49px', 'top': '291.81px' }, { 'left': '522.49px', 'top': '333.31px' }, { 'left': '564.55px', 'top': '291.81px' }, { 'left': '634.71px', 'top': '307.05px' }, { 'left': '587.25px', 'top': '282.37px' },
+{ 'left': '573.38px', 'top': '295.39px' }, { 'left': '582.27px', 'top': '315.29px' }, { 'left': '582.27px', 'top': '338.50px' }, { 'left': '574.43px', 'top': '387.48px' }, { 'left': '539.56px', 'top': '422.02px' },
+{ 'left': '612.78px', 'top': '386.44px' }, { 'left': '598.74px', 'top': '400.88px' }, { 'left': '634.71px', 'top': '338.50px' }, { 'left': '651.46px', 'top': '326.02px' }, { 'left': '670.29px', 'top': '326.02px' },
+{ 'left': '438.48px', 'top': '427.47px' }, { 'left': '410.53px', 'top': '427.47px' }, { 'left': '495.64px', 'top': '358.76px' }, { 'left': '459.09px', 'top': '402.59px' }, { 'left': '410.53px', 'top': '466.70px' },
+{ 'left': '623.64px', 'top': '466.09px' }, { 'left': '510.51px', 'top': '597.49px' }, { 'left': '517.36px', 'top': '547.21px' }, { 'left': '546.14px', 'top': '471.34px' }, { 'left': '517.36px', 'top': '502.81px' },
+{ 'left': '500.67px', 'top': '502.81px' }, { 'left': '438.48px', 'top': '471.63px' }, { 'left': '438.48px', 'top': '485.55px' }, { 'left': '450.90px', 'top': '519.86px' }, { 'left': '457.64px', 'top': '519.86px' },
+{ 'left': '474.96px', 'top': '579.82px' }, { 'left': '510.51px', 'top': '579.82px' }, { 'left': '599.67px', 'top': '585.02px' }, { 'left': '594.43px', 'top': '585.02px' }, { 'left': '545.01px', 'top': '641.88px' },
+{ 'left': '639.70px', 'top': '704.84px' }, { 'left': '594.43px', 'top': '547.21px' }, { 'left': '594.43px', 'top': '566.41px' }, { 'left': '627.21px', 'top': '538.05px' }, { 'left': '603.04px', 'top': '471.34px' },
+{ 'left': '603.04px', 'top': '538.05px' }, { 'left': '650.64px', 'top': '538.05px' }, { 'left': '627.21px', 'top': '791.04px' }, { 'left': '583.37px', 'top': '745.82px' }, { 'left': '668.77px', 'top': '523.41px' },
+{ 'left': '671.33px', 'top': '523.41px' }, { 'left': '671.33px', 'top': '502.10px' }, { 'left': '659.74px', 'top': '437.09px' }, { 'left': '659.74px', 'top': '469.77px' }, { 'left': '638.69px', 'top': '498.44px' },
+{ 'left': '645.84px', 'top': '469.77px' }, { 'left': '641.13px', 'top': '432.81px' }, { 'left': '634.76px', 'top': '432.81px' }, { 'left': '641.13px', 'top': '432.81px' }, { 'left': '673.08px', 'top': '304.65px' },
+{ 'left': '676.27px', 'top': '304.65px' }, { 'left': '676.27px', 'top': '277.61px' }, { 'left': '669.81px', 'top': '263.85px' }, { 'left': '650.25px', 'top': '286.45px' }, { 'left': '659.55px', 'top': '263.85px' },
+]
+
+
+    vector_ids = [
+'vector-0','vector-1','vector-2','vector-3','vector-4','vector-5','vector-6','vector-7','vector-8','vector-9',
+'vector-10','vector-11','vector-12','vector-13','vector-14','vector-15','vector-16','vector-17','vector-18','vector-19',
+'vector-20','vector-21','vector-22','vector-23','vector-24','vector-25','vector-26','vector-27','vector-28','vector-29',
+'vector-30','vector-31','vector-32','vector-33','vector-34','vector-35','vector-36','vector-37','vector-38','vector-39',
+'vector-40','vector-41','vector-42','vector-43','vector-44','vector-45','vector-46','vector-47','vector-48','vector-49',
+'vector-50','vector-51','vector-52','vector-53','vector-54','vector-55','vector-56','vector-57','vector-58','vector-59',
+'vector-60','vector-61','vector-62','vector-63','vector-64','vector-65','vector-66','vector-67','vector-68','vector-69',
+'vector-70','vector-71','vector-72','vector-73','vector-74','vector-75','vector-76','vector-77','vector-78','vector-79',
+'vector-80','vector-81','vector-82','vector-83','vector-84','vector-85','vector-86','vector-87','vector-88','vector-89',
+'vector-90','vector-91','vector-92','vector-93','vector-94','vector-95','vector-96','vector-97','vector-98','vector-99',
+'vector-100','vector-101','vector-102','vector-103','vector-104','vector-105','vector-106','vector-107','vector-108','vector-109',
+'vector-110','vector-111','vector-112','vector-113','vector-114','vector-115','vector-116','vector-117','vector-118','vector-119',
+'vector-120','vector-121','vector-122','vector-123','vector-124','vector-125','vector-126','vector-127','vector-128', 'vector-129'
+]
+
+
+function assemble_israel() {
+    vector_elements = vector_ids.map( (i) => { return document.getElementById(i); } );
+
+    for(let i in vector_elements) {
+        vector_elements[i].animate([ end_pos[i] ],
+            { duration: 2000, fill: "forwards"}
+        );
+    }
+}
+
+
