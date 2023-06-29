2aa0927a0b7a3af3b90fa93bcf877dcb7ec18823
ShaharTamir <shahar.tamir@infinityLabs.co.il>
2023-06-30 00:09:28 +0300

    small formats and add shapes circle with blurred background which glows on hover

--- static/javascript/reef_destruction.js
+++ static/javascript/reef_destruction.js
@@ -1,3 +1,49 @@
+end_pos = [
+{ 'width': '20.80px', 'height': '16.76px', 'left': '543.92px', 'top': '370.21px' }, { 'width': '21.86px', 'height': '21.74px', 'left': '813.59px', 'top': '101.55px' }, { 'width': '32.66px', 'height': '97.83px', 'left': '587.67px', 'top': '283.33px' },
+{ 'width': '21.05px', 'height': '38.52px', 'left': '509.88px', 'top': '551.34px' }, { 'width': '28.58px', 'height': '21.76px', 'left': '523.41px', 'top': '427.96px' }, { 'width': '46.88px', 'height': '48.18px', 'left': '573.22px', 'top': '456.45px' },
+{ 'width': '37.95px', 'height': '42.31px', 'left': '645.06px', 'top': '298.97px' }, { 'width': '64.46px', 'height': '108.36px', 'left': '1019.05px', 'top': '89.34px' }, { 'width': '32.42px', 'height': '20.26px', 'left': '644.16px', 'top': '235.03px' },
+{ 'width': '97.08px', 'height': '84.59px', 'left': '653.84px', 'top': '183.76px' }, { 'width': '48.78px', 'height': '44.75px', 'left': '660.94px', 'top': '260.35px' }, { 'width': '27.26px', 'height': '47.06px', 'left': '900.42px', 'top': '151.59px' },
+{ 'width': '32.21px', 'height': '81.88px', 'left': '748.19px', 'top': '144.26px' }, { 'width': '27.76px', 'height': '20.45px', 'left': '585.60px', 'top': '524.48px' }, { 'width': '16.84px', 'height': '28.43px', 'left': '591.08px', 'top': '401.62px' },
+{ 'width': '90.26px', 'height': '105.94px', 'left': '830.83px', 'top': '105.77px' }, { 'width': '29.62px', 'height': '60.75px', 'left': '711.69px', 'top': '153.85px' }, { 'width': '108.06px', 'height': '86.26px', 'left': '944.15px', 'top': '91.60px' },
+{ 'width': '16.19px', 'height': '76.15px', 'left': '517.38px', 'top': '465.61px' }, { 'width': '10.20px', 'height': '12.96px', 'left': '700.87px', 'top': '169.41px' }, { 'width': '9.53px', 'height': '260.70px', 'left': '625.77px', 'top': '228.77px' },
+{ 'width': '32.09px', 'height': '50.84px', 'left': '943.37px', 'top': '79.22px' }, { 'width': '43.53px', 'height': '40.80px', 'left': '576.60px', 'top': '583.84px' }, { 'width': '148.76px', 'height': '202.75px', 'left': '749.83px', 'top': '72.07px' },
+{ 'width': '59.48px', 'height': '327.65px', 'left': '529.96px', 'top': '328.30px' }, { 'width': '38.84px', 'height': '36.52px', 'left': '1311.75px', 'top': '591.63px' }, { 'width': '78.08px', 'height': '111.41px', 'left': '1335.46px', 'top': '455.61px' },
+{ 'width': '41.74px', 'height': '44.13px', 'left': '1299.61px', 'top': '339.67px' }, { 'width': '29.14px', 'height': '26.57px', 'left': '1398.11px', 'top': '461.29px' }, { 'width': '40.94px', 'height': '42.04px', 'left': '1370.17px', 'top': '414.40px' },
+{ 'width': '28.30px', 'height': '79.76px', 'left': '1387.96px', 'top': '574.01px' }, { 'width': '162.31px', 'height': '174.29px', 'left': '1263.31px', 'top': '348.55px' }, { 'width': '8.82px', 'height': '8.93px', 'left': '1362.19px', 'top': '349.49px' },
+{ 'width': '24.48px', 'height': '31.86px', 'left': '1334.73px', 'top': '297.61px' }, { 'width': '10.31px', 'height': '61.70px', 'left': '1316.70px', 'top': '514.64px' }, { 'width': '52.44px', 'height': '24.18px', 'left': '1335.27px', 'top': '372.11px' },
+{ 'width': '91.11px', 'height': '225.89px', 'left': '1308.48px', 'top': '448.59px' }, { 'width': '19.91px', 'height': '21.00px', 'left': '1341.40px', 'top': '632.53px' }, { 'width': '31.35px', 'height': '28.79px', 'left': '1242.24px', 'top': '290.56px' },
+{ 'width': '25.61px', 'height': '22.17px', 'left': '1203.10px', 'top': '263.09px' }, { 'width': '92.98px', 'height': '43.86px', 'left': '1205.66px', 'top': '208.20px' }, { 'width': '27.90px', 'height': '47.34px', 'left': '1127.63px', 'top': '118.38px' },
+{ 'width': '87.22px', 'height': '33.04px', 'left': '1180.12px', 'top': '179.73px' }, { 'width': '14.74px', 'height': '13.94px', 'left': '1263.44px', 'top': '270.01px' }, { 'width': '29.81px', 'height': '72.76px', 'left': '1159.49px', 'top': '125.55px' },
+{ 'width': '36.84px', 'height': '43.05px', 'left': '1104.90px', 'top': '175.00px' }, { 'width': '195.32px', 'height': '54.41px', 'left': '1124.29px', 'top': '223.31px' }, { 'width': '91.26px', 'height': '39.04px', 'left': '683.34px', 'top': '816.36px' },
+{ 'width': '94.95px', 'height': '71.53px', 'left': '580.31px', 'top': '656.14px' }, { 'width': '79.48px', 'height': '34.42px', 'left': '862.83px', 'top': '881.84px' }, { 'width': '33.81px', 'height': '44.18px', 'left': '564.78px', 'top': '721.48px' },
+{ 'width': '62.21px', 'height': '90.51px', 'left': '591.62px', 'top': '751.39px' }, { 'width': '55.96px', 'height': '31.11px', 'left': '752.45px', 'top': '914.70px' }, { 'width': '61.53px', 'height': '37.14px', 'left': '799.91px', 'top': '869.97px' },
+{ 'width': '23.30px', 'height': '55.29px', 'left': '921.56px', 'top': '929.82px' }, { 'width': '191.12px', 'height': '86.77px', 'left': '700.41px', 'top': '868.57px' }, { 'width': '283.55px', 'height': '174.26px', 'left': '550.74px', 'top': '685.67px' },
+{ 'width': '22.76px', 'height': '27.76px', 'left': '832.45px', 'top': '948.98px' }, { 'width': '34.50px', 'height': '29.71px', 'left': '1038.72px', 'top': '877.79px' }, { 'width': '68.55px', 'height': '108.42px', 'left': '1195.17px', 'top': '742.82px' },
+{ 'width': '37.13px', 'height': '25.16px', 'left': '1013.83px', 'top': '920.91px' }, { 'width': '82.05px', 'height': '69.77px', 'left': '1292.03px', 'top': '709.43px' }, { 'width': '56.44px', 'height': '84.80px', 'left': '1074.08px', 'top': '887.85px' },
+{ 'width': '87.46px', 'height': '105.56px', 'left': '1130.09px', 'top': '849.58px' }, { 'width': '9.47px', 'height': '16.85px', 'left': '1276.33px', 'top': '826.42px' }, { 'width': '12.93px', 'height': '21.41px', 'left': '1058.81px', 'top': '956.87px' },
+{ 'width': '65.62px', 'height': '243.06px', 'left': '1234.99px', 'top': '658.22px' }, { 'width': '12.14px', 'height': '23.65px', 'left': '1082.96px', 'top': '856.39px' }, { 'width': '23.61px', 'height': '23.09px', 'left': '1304.18px', 'top': '793.32px' },
+{ 'width': '21.31px', 'height': '23.02px', 'left': '975.19px', 'top': '965.30px' }, { 'width': '18.67px', 'height': '28.33px', 'left': '991.48px', 'top': '919.19px' }, { 'width': '169.33px', 'height': '179.30px', 'left': '1006.19px', 'top': '817.91px' },
+{ 'width': '39.21px', 'height': '25.88px', 'left': '984.04px', 'top': '883.25px' }, { 'width': '29.69px', 'height': '16.42px', 'left': '1301.31px', 'top': '684.11px' }, { 'width': '17.85px', 'height': '13.70px', 'left': '1289.11px', 'top': '769.09px' },
+{ 'width': '48.94px', 'height': '43.39px', 'left': '1348.38px', 'top': '679.79px' },
+]
+
+vectors = [
+'vector-0','vector-1','vector-2','vector-3','vector-4','vector-5',
+'vector-6','vector-7','vector-8','vector-9','vector-10','vector-11',
+'vector-12','vector-13','vector-14','vector-15','vector-16','vector-17',
+'vector-18','vector-19','vector-20','vector-21','vector-22','vector-23',
+'vector-24','vector-25','vector-26','vector-27','vector-28','vector-29',
+'vector-30','vector-31','vector-32','vector-33','vector-34','vector-35',
+'vector-36','vector-37','vector-38','vector-39','vector-40','vector-41',
+'vector-42','vector-43','vector-44','vector-45','vector-46','vector-47',
+'vector-48','vector-49','vector-50','vector-51','vector-52','vector-53',
+'vector-54','vector-55','vector-56','vector-57','vector-58','vector-59',
+'vector-60','vector-61','vector-62','vector-63','vector-64','vector-65',
+'vector-66','vector-67','vector-68','vector-69','vector-70','vector-71',
+'vector-72','vector-73','vector-74','vector-75',
+]
+
+
 var seconds = 0;
 var dead_reefs_per_sec = 250;
 var interval = 2000;
@@ -9,4 +55,30 @@ function incrementSeconds() {
     el.innerText = dead_reefs_per_sec * seconds;
 }
 
-setInterval(incrementSeconds, 2000);
+setInterval(incrementSeconds, 1000);
+
+
+function assemble_circle() {
+    elements = vectors.map(i => document.getElementById(i));
+    background = document.getElementById('background-image');
+    blur_circle = document.getElementById('blur-circle');
+
+    for (elem in elements) {
+        elements[elem].animate([end_pos[elem]], {duration: 1000, easing: "ease-in-out", fill: "forwards"});
+        elements[elem].animate([{"filter":"grayscale(0)"}], {duration: 1200, fill: "forwards"});
+    }
+
+    background.animate([{"opacity": "1"}], {duration: 1000, fill: "forwards"});
+    blur_circle.animate([{"opacity": "1"}], {duration: 1000, fill: "forwards"});
+    background.style.zIndex = 5;
+}
+
+function blur_shine(group_num, is_in) {
+    circle_shapes_ids = ["blur-0", "blur-1", "blur-2", "blur-3", "blur-4"];
+    elements = circle_shapes_ids.map(i => document.getElementById(i));
+    opacity = is_in ? 0.5 : 0.3;
+
+    elements[group_num].style.opacity = opacity;
+}
+
+
