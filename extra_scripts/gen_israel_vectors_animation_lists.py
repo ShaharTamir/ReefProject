1cde90a8e771686636291a211c57acd8b06dd730
ShaharTamir <shahar.tamir@infinityLabs.co.il>
2023-06-29 16:12:47 +0300

    finish reef importance for real...

--- extra_scripts/gen_israel_vectors_animation_lists.py
+++ extra_scripts/gen_israel_vectors_animation_lists.py
@@ -7,20 +7,25 @@ mark all vectors in Figma at ending position and copy the generated code into en
 It is recommended to group each vector, then flatten them before usage for more accurate positioning.
 """
 
+WIDTH = 0
+HEIGHT = 1
 LEFT = 2
 TOP = 3
-LINE_BREAK = 5
+LINE_BREAK = 3
 
-def str_field_name_and_value(attribute):
+def str_field_name_and_value(attribute, plus=0):
     pieces = attribute.split(":")
-    return f"'{pieces[0]}': '{pieces[1].strip()}'"
+    return f"'{pieces[0]}': '{float(pieces[1].strip()[:-2:]) + plus:.2f}px'"
 
 
 def gen_positions_arr(formatted_html_line):
     for line in range(0, len(formatted_html_line)):
         attributes = formatted_html_line[line].split(";")
+        print(attributes)
         s = "\n{ " if line % LINE_BREAK == 0 else "{ " # line_break defined in global scope
-        output_file.write(s + f"{str_field_name_and_value(attributes[LEFT].strip())}, \
+        output_file.write(s + f"{str_field_name_and_value(attributes[WIDTH].strip(), 12)}, \
+{str_field_name_and_value(attributes[HEIGHT].strip(), 3)}, \
+{str_field_name_and_value(attributes[LEFT].strip())}, \
 {str_field_name_and_value(attributes[TOP].strip())}" + " }, ")
 
     output_file.write("\n]\n\n[")
@@ -30,18 +35,18 @@ def extract_basic_format_html(filename):
         txt = file.readlines()
     return [line.split("\"")[1] for line in txt]
 
-formatted_start_lines = extract_basic_format_html("start_pos.txt")
+# formatted_start_lines = extract_basic_format_html("start_pos.txt")
 formatted_end_lines = extract_basic_format_html("end_pos.txt")
 
 with open("formatted_positions.txt", "w") as output_file:
     output_file.write("[")
 
 
-    gen_positions_arr(formatted_start_lines)
+  #  gen_positions_arr(formatted_start_lines)
     gen_positions_arr(formatted_end_lines)
 
     # gen list of vectors ids
-    for line in range(0, len(formatted_start_lines)):
+    for line in range(0, len(formatted_end_lines)):
         s = "\n" if line % (LINE_BREAK * 2) == 0 else ""
         output_file.write(s + f"'vector-{line}',")
     output_file.write("\n]\n\n")
